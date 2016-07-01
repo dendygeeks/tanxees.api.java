0a4e2c7e597858d21536c6d37a455bbb331f21c0
Vasily <just.one.man@yandex.ru>
2016-07-02 01:42:22 +0300

    Added parameters for modeling pseudo-physical tank movement

--- src/main/java/dendygeeks/tanxees/api/java/interfaces/CellType.java
+++ src/main/java/dendygeeks/tanxees/api/java/interfaces/CellType.java
@@ -1,18 +1,22 @@
 package dendygeeks.tanxees.api.java.interfaces;
 
 public enum CellType {
-	EMPTY("E"),
-	CONCRETE("C"),
-	BRICKS("B"),
-	TREE("T"),
-	DARK_CONCRETE("DC"),
-	DARK_BRICKS("DB"),
-	WATER("W");
+	EMPTY("E", 1.0, 0.3), //10.0, 30.0),
+	CONCRETE("C", 0.0, 0.0),
+	BRICKS("B", 0.0, 0.0),
+	TREE("T", 2.0, 1.0),
+	DARK_CONCRETE("DC", 0.0, 0.0),
+	DARK_BRICKS("DB", 0.0, 0.0),
+	WATER("W", 0.0, 0.0),
+	ICE("I", 1.0, 0.3);
 	
 	public final String code;
+	public final double dryFriction, airFriction;
 	
-	CellType(String code) {
+	CellType(String code, double dryFriction, double airFriction) {
 		this.code = code;
+		this.dryFriction = dryFriction;
+		this.airFriction = airFriction;
 	}
 	
 	public static CellType fromCode(String code) {
--- src/main/java/dendygeeks/tanxees/api/java/interfaces/UnitType.java
+++ src/main/java/dendygeeks/tanxees/api/java/interfaces/UnitType.java
@@ -1,10 +1,11 @@
 package dendygeeks.tanxees.api.java.interfaces;
 public enum UnitType {
 
-	SMALL("small", 20/*22*/, 27), MEDIUM("medium", 34, 34);
+	SMALL("small", 20/*22*/, 27, 1.0, 1.0, 1.0, 0.7), MEDIUM("medium", 34, 34, 5.0, 1.0, 300.0, 200.0);
 
 	public final String id;
 	public final double sizeW, sizeL;
+	public final double mass, midship, forwardPower, backwardPower;
 
 	public static UnitType fromId(String id) {
 		for (UnitType val : values()) {
@@ -13,9 +14,13 @@ public enum UnitType {
 		return null;
 	}
 	
-	UnitType(String id, double sizeW, double sizeL) {
+	UnitType(String id, double sizeW, double sizeL, double mass, double midship, double forwardPower, double backwardPower) {
 		this.id = id;
 		this.sizeW = sizeW;
 		this.sizeL = sizeL;
+		this.mass = mass;
+		this.midship = midship;
+		this.forwardPower = forwardPower;
+		this.backwardPower = backwardPower;
 	}
 }

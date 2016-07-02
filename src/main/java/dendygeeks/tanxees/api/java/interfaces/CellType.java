ea2fc16933c1f8be117ce8af09d67a2a4ba8e3f6
Ilya Mizus <bigfatbrowncat@gmail.com>
2016-07-02 21:52:19 +0300

    Fixed physical parameters

--- src/main/java/dendygeeks/tanxees/api/java/interfaces/CellType.java
+++ src/main/java/dendygeeks/tanxees/api/java/interfaces/CellType.java
@@ -4,7 +4,7 @@ public enum CellType {
 	EMPTY("E", 12.0, 30.0),
 	CONCRETE("C", 0.0, 0.0),
 	BRICKS("B", 0.0, 0.0),
-	TREE("T", 2.0, 1.0),
+	TREE("T", 12.0, 30.0),
 	DARK_CONCRETE("DC", 0.0, 0.0),
 	DARK_BRICKS("DB", 0.0, 0.0),
 	WATER("W", 0.0, 0.0),
